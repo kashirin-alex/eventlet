0bc077cc779f2523f661f31770ccfb8a942c9f61
Kashirin Alex <kashirin.alex@gmail.com>
2018-08-27 10:10:03 +0300

    use of heapq with an optimal loop on Hub
    Signed-off-by: Kashirin Alex <kashirin.alex@gmail.com>

--- eventlet/hubs/hub.py
+++ eventlet/hubs/hub.py
@@ -453,6 +453,7 @@ class BaseHub(object):
 
             try:
                 tmr()
+                return 0
             except self.SYSTEM_EXCEPTIONS:
                 raise
             except:
