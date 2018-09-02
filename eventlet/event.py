db5fa27a6fb03b9d7f564c5a482b172cab355b54
Kashirin Alex <kashirin.alex@gmail.com>
2018-09-03 01:21:51 +0300

    not just True but bool(exc)
    Signed-off-by: Kashirin Alex <kashirin.alex@gmail.com>
    
    #408

--- eventlet/event.py
+++ eventlet/event.py
@@ -163,6 +163,7 @@ class Event(object):
         self._result = result
         if exc is not None and not isinstance(exc, tuple):
             exc = (exc, )
+            
         self._exc = bool(exc)
         for waiter in self._waiters:
             active_hub.inst.schedule_call_global(
