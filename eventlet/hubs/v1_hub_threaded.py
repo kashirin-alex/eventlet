82a776451e7d01547cb640fa5dc35dd0b7ec7ea5
Kashirin Alex <kashirin.alex@gmail.com>
2018-09-12 01:34:33 +0300

    seq. events processing for timers and listeners
    Signed-off-by: Kashirin Alex <kashirin.alex@gmail.com>

--- eventlet/hubs/v1_hub_threaded.py
+++ eventlet/hubs/v1_hub_threaded.py
@@ -87,7 +87,6 @@ class BaseHub(HubBase):
                     exp, event = events[0]
                     if isinstance(event, self.lclass):
                         typ = 1
-                        print (exp, typ)
                     else:
                         if event.called:
                             # remove called/cancelled timer
@@ -99,6 +98,7 @@ class BaseHub(HubBase):
                     if due > 0:
                         chk = False
                         ev_sleep(0)
+                        print (typ, exp, )
                         continue
                     delay = (due + delay) / 2  # delay is negative value
 
