4e5cd9d8dfc6e692dbd74f2f17b903efb7bb64f2
Kashirin Alex <kashirin.alex@gmail.com>
2018-09-13 12:03:23 +0300

    sequential events schedule,  timers and fd events evaluated together
    Signed-off-by: Kashirin Alex <kashirin.alex@gmail.com>

--- eventlet/hubs/standalone_epolls.py
+++ eventlet/hubs/standalone_epolls.py
@@ -43,15 +43,13 @@ class Hub(HubSkeleton):
         self.events = []
         self.next_events = []
         self.add_next_event = self.next_events.append
-        self.next_timers = []
-        self.add_next_timers = self.next_timers.append
 
         self.poll = select.epoll()
         #
 
     def add_timer(self, timer):
         timer.scheduled_time = self.clock() + timer.seconds
-        self.add_next_timers(timer)
+        self.add_next_event((timer.scheduled_time, timer))
         return timer
         #
 
@@ -121,14 +119,10 @@ class Hub(HubSkeleton):
         self.stopping = False
 
         clock = self.clock
-        delay = 0
         events = self.events
-
-        next_timers = self.next_timers
-        pop_next_timer = self.next_timers.pop
-
         next_events = self.next_events
         pop_next_event = self.next_events.pop
+        delay = 0
 
         closed = self.closed
         pop_closed = self.closed.pop
@@ -149,9 +143,6 @@ class Hub(HubSkeleton):
                 if not l.greenlet.dead:  # There's no point signalling a greenlet that's already dead.
                     l.tb(eventlet.hubs.IOClosed(errno.ENOTCONN, "Operation on closed file"))
 
-            while next_timers:
-                t = pop_next_timer(-1)
-                heappush(events, (t.scheduled_time, t))
             while next_events:
                 heappush(events, pop_next_event(-1))
 
