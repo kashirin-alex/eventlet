5eb7b871752a2573e52c96e2797a38866406c278
Kashirin Alex <kashirin.alex@gmail.com>
2018-09-14 09:12:27 +0300

    listeners can be tuple
    Signed-off-by: Kashirin Alex <kashirin.alex@gmail.com>

--- eventlet/green/select.py
+++ eventlet/green/select.py
@@ -74,14 +74,15 @@ def select(read_list, write_list, error_list, timeout=None):
         timers.append(hub.schedule_call_global(timeout, on_timeout))
 
     throw = current.throw
-    listeners = [
-        hub.add(ev, fileno, on, throw, lambda: None)
+    add_listener = hub.add
+    listeners = (
+        add_listener(ev, fileno, on, throw, lambda: None)
         for fds, ev, on in (
             (ds_read, hub.READ, on_read),
             (ds_write, hub.WRITE, on_write)
         )
         for fileno in fds
-    ]
+    )
 
     try:
         try:
